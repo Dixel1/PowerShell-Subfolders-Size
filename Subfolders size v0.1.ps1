ec1e5523c5450e488033ce1e305f302e0da3a745
Sarvagon <63664894+Sarvagon@users.noreply.github.com>
2022-10-06 10:03:08 +0200

    Add files via upload

---
+++ Subfolders size v0.1.ps1
@@ -0,0 +1,11 @@
+$startFolder = "C:\Users\stagiaire";
+$totalSize = 0;
+
+$colItems = (Get-ChildItem $startFolder -recurse | Where-Object {$_.PSIsContainer -eq $True} | Sort-Object)
+foreach ($i in $colItems)
+    {
+        $subFolderItems = (Get-ChildItem $i.FullName | Measure-Object -property length -sum)
+        $i.FullName + " -- " + "{0:N2}" -f ($subFolderItems.sum / 1MB) + " MB"
+    }
+
+$startFolder + " | " + "{0:N2}" -f ($totalSize) + " MB"
